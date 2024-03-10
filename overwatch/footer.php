2cf35f5c579002addd69abf905fb6ea03cbeb904
teegee6t9 <godard.thibault@gmail.com>
2024-03-10 15:11:22 -0400

    Projet-liste - Partie publique

---
+++ overwatch/footer.php
@@ -0,0 +1,35 @@
+<footer>
+      <ul class="social-icon">
+        <li>
+          <a href="www.discord.com"
+            ><i class="fa-brands fa-discord" style="color: none"></i
+          ></a>
+        </li>
+        <li>
+          <a href="www.twitter.com"
+            ><i class="fa-brands fa-twitter" style="color: none"></i
+          ></a>
+        </li>
+        <li>
+          <a href="www.github.com"
+            ><i class="fa-brands fa-github" style="color: none"></i
+          ></a>
+        </li>
+        <li>
+          <a href="www.instagram.com"
+            ><i class="fa-brands fa-instagram" style="color: none"></i
+          ></a>
+        </li>
+        <li>
+          <a href="www.twitch.tv"
+            ><i class="fa-brands fa-twitch" style="color: none"></i
+          ></a>
+        </li>
+      </ul>
+      <div class="copyright">
+        Copyright &copy; 2024 Thibault Godard. Tout droits
+        réservés
+      </div>
+    </footer>
+</body>
+</html>
