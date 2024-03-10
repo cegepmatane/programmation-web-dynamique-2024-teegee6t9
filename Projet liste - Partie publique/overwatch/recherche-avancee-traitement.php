9115afc6f88651ec4036d3f4e4c607848cb99145
teegee6t9 <godard.thibault@gmail.com>
2024-03-10 15:18:46 -0400

    Projet liste - Partie publique

---
+++ Projet liste - Partie publique/overwatch/recherche-avancee-traitement.php
@@ -0,0 +1,70 @@
+<?php
+$resultats = [];
+$nomRecherche = $_GET['recherche-nom'];
+$classeRecherche = $_GET['recherche-classe'];
+$pvRecherche = $_GET['recherche-pv'];
+$ultimateRecherche = $_GET['recherche-ultimate'];
+
+if (!empty($nomRecherche) || !empty($classeRecherche) || !empty($pvRecherche) || !empty($ultimateRecherche)) {
+    $SQL_RECHERCHE_AVANCEE = "SELECT * FROM heros WHERE 1 = 1 ";
+
+    if (!empty($nomRecherche)) {
+        $SQL_RECHERCHE_AVANCEE = $SQL_RECHERCHE_AVANCEE . " AND LOWER(nom) LIKE LOWER('%$nomRecherche%')";
+    }
+    if (!empty($classeRecherche)) {
+        $SQL_RECHERCHE_AVANCEE = $SQL_RECHERCHE_AVANCEE . " AND LOWER(classe) LIKE LOWER('%$classeRecherche%')";
+    }
+    if (!empty($pvRecherche)) {
+        $SQL_RECHERCHE_AVANCEE = $SQL_RECHERCHE_AVANCEE . " AND LOWER(pv) LIKE LOWER('%$pvRecherche%')";
+    }
+    if (!empty($ultimateRecherche)) {
+        $SQL_RECHERCHE_AVANCEE = $SQL_RECHERCHE_AVANCEE . "AND LOWER(habilite1) LIKE LOWER('%$ultimateRecherche%') OR LOWER(habilite2) LIKE LOWER('%$ultimateRecherche%') OR LOWER(ultimate) LIKE LOWER('%$ultimateRecherche%')";
+    }
+
+    include "basededonnees.php";
+
+    $requeteRecherche = $basededonnees->prepare($SQL_RECHERCHE_AVANCEE);
+    $requeteRecherche->execute();
+    $resultats = $requeteRecherche->fetchAll();
+}
+$titre = "Recherche avancée";
+require "header.php";
+?>
+
+<link rel="stylesheet" href="style/liste-heros.css"/>
+
+<div class="container">
+        <h1>Overwatch</h1>
+        <?php
+if (count($resultats) != 0) {
+    ?>
+
+        <h2><?="Nombre de réslutat : " . count($resultats)?></h2>
+        <?php
+} else {
+    ?>
+    <h2>Résultat trouvé : Aucun</h2>
+    <?php
+}
+?>
+
+        <div id="liste-heros"></div>
+        <?php
+foreach ($resultats as $resultats) {
+    ?>
+    <div class="heros">
+        <div class="images"><img src="images/mini/<?=$resultats['icon']?>" alt="illustration"></div>
+        <h3 class="nom"><?=$resultats['nom']?></h3>
+        <span class="classe"><?=$resultats['classe']?></span>
+        <span class="pv"><?=$resultats['pv']?></span>
+        <p class="description_courte"><?=$resultats['description_courte']?></p>
+    </div>
+
+            <?php
+
+}
+?>
+
+<?php
+require 'footer.php';
+?>
